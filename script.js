a0af77e81aceba81d9e0f32ffcffa2ac86849e4e
Gagan <gagandodiya07@gmail.com>
2022-05-23 17:02:21 +0530

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function validate(){
+    var username = document.loginform.user.value;
+    var password = document.loginform.pass.value;
+
+ username = username.trim();
+ password = password.trim();
+
+ if(username == null || username =='')
+ {
+     alert("Username cannot be empty");
+     return false;
+ }
+ if(password.length<6){
+     alert("Password must be atleast of 6 characters");
+     return false;
+ }
+ return true;
+ 
+
+
+}
